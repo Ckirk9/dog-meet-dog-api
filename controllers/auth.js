57c37a4f95702ef1fa0c15f0b506ceacb28a4e78
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-09-16 15:36:19 -0400

    updated file structure for backend and completed auth.js in controllers folder

---
+++ controllers/auth.js
@@ -0,0 +1,59 @@
+const db = require('../models')
+const bcrypt = require('bcrypt')
+
+const login = (req, res) => {
+    console.log('req.pet:', req.pet)
+    console.log('req.session:', req.session)
+    res.json({ user: req.pet.username })
+}
+
+const signUp = (req, res) => {
+    const { username, password } = req.body 
+    if (!username || !password) {
+        return res.json({
+            message: 'Please enter a username and a password'
+        })
+    }
+    // make sure pet doesn't already have an account 
+    db.Pet.findOne({ username: username }, (err, foundPet) => {
+        if (err)     
+            return res.json({
+                message: 'error'
+            })
+        
+        if (foundPet) 
+            return res.json({
+                message: 'this username is already in use'
+            })
+        
+        const newPet = new db.Pet({
+            username,
+            password,
+            bio,
+            pictureUrl
+        })
+        console.log("Begin save pet");
+        newPet.save((err, savedPet) => {
+            if (err) {
+                res.json(err)
+            }
+            delete savedPet.password
+            res.json(savedPet)
+        })
+        console.log("End save pet");
+    })
+}
+
+const signOut = (req, res) => {
+    if (!req.pet) return res.json({
+        message: 'no pet to sign out'
+    })
+    req.logout()
+    res.json({ message: 'pet signed out'})
+}
+
+module.exports = {
+    login, 
+    signUp,
+    signOut, 
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 
+
 // App config
 const app = express()
 const port = process.env.PORT || 5000
@@ -18,6 +19,7 @@ mongoose.connect(connection_url, {
 
 // API endpoints
 app.get('/', (req, res) => res.status(200).send("sanity check"))
+
 
 // Listener 
 app.listen(port, () => console.log(`listening on localhost: ${port}`))
